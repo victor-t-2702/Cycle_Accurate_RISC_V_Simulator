eabb3acd4cb1bcadc5ebcbd04a7e161f42908f67
vgt1 <vgt1@sfu.ca>
2025-08-07 12:48:50 -0700

    Edited config.h for milestone 2

--- config.h
+++ config.h
@@ -9,23 +9,23 @@
 // #define DEBUG_CYCLE
 //#define MEM_LATENCY 0		// before ms3, we ignore memory access latency
 
-// required for MS2 (test_simulator_ms2.sh)
-// #define DEBUG_REG_TRACE
-// #define DEBUG_CYCLE
-// #define PRINT_STATS		// prints overall stats
-// #define MEM_LATENCY 0
-
-// required for MS2: vec_xprod.input (test_simulator_ms2_extended.sh)
-//#define PRINT_STATS
-//#define MEM_LATENCY 0
-
-// required for MS3: (test_simulator_ms3.sh)
+//required for MS2 (test_simulator_ms2.sh)
 #define DEBUG_REG_TRACE
 #define DEBUG_CYCLE
+#define PRINT_STATS		// prints overall stats
+#define MEM_LATENCY 0
+
+// required for MS2: vec_xprod.input (test_simulator_ms2_extended.sh)
 #define PRINT_STATS
-#define MEM_LATENCY 100
-#define CACHE_ENABLE 		// enable cache simulation
-#define PRINT_CACHE_TRACES      // prints cache trace for each memory access 
-#define PRINT_CACHE_STATS	// prints the cache stats at the end of program
+#define MEM_LATENCY 0
+
+// required for MS3: (test_simulator_ms3.sh)
+//#define DEBUG_REG_TRACE
+//#define DEBUG_CYCLE
+//#define PRINT_STATS
+//#define MEM_LATENCY 100
+//#define CACHE_ENABLE 		// enable cache simulation
+//#define PRINT_CACHE_TRACES      // prints cache trace for each memory access 
+//#define PRINT_CACHE_STATS	// prints the cache stats at the end of program
 
 #endif // __CONFIG_H__
